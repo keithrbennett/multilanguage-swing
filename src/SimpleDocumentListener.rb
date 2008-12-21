3828dd4d8e30e96e6459a4e6deda2d190ae5f563
Keith R. Bennett <kbennett@bbsinc.biz>
2008-12-21 17:36:03 -0500

    Added new file SimpleDocumentListener.

---
+++ src/SimpleDocumentListener.rb
@@ -0,0 +1,32 @@
+# Simple implementation of javax.swing.event.DocumentListener that
+# performs the same behavior regardless of which of the three
+# methods are called.
+
+require 'java'
+
+import javax.swing.event.DocumentListener
+
+
+class SimpleDocumentListener
+  
+  include DocumentListener
+  
+  attr_accessor :code_block
+  
+  def initialize(code_block)
+    this.code_block = code_block
+  end
+  
+  def changedUpdate(event)
+    code_block.call event
+  end
+  
+  def insertUpdate(event)
+    code_block.call event
+  end
+  
+  def removeUpdate(event)
+    code_block.call event
+  end
+  
+end
