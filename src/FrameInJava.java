4e985a9f40d13c04aefb5690036a360ca5c99129
Keith R. Bennett <kbennett@bbsinc.biz>
2009-02-24 00:41:19 -0500

    Much refactoring and reformatting.

--- src/FrameInJava.java
+++ src/FrameInJava.java
@@ -34,7 +34,7 @@ import javax.swing.event.DocumentListener;
  * Main application frame containing menus, text fields for the temperatures,
  * and buttons.  The menu items and buttons are driven by shared actions,
  * which are disabled and enabled based on the state of the text fields.
- * 
+ *
  * Temperatures can be converted in either direction, Fahrenheit to Celsius
  * or Celsius to Fahrenheit.  The convert actions (F2C, C2F) are each enabled
  * when the respective source text field (F for F2C, C for C2F) contains text
@@ -48,41 +48,87 @@ public class FrameInJava extends JFrame {
     private JTextField celsTextField = new JTextField(15);
 
     // These actions will be shared by menu items and buttons.
-    private AbstractAction f2cAction;
-    private AbstractAction c2fAction;
-    private AbstractAction clearAction;
-    private AbstractAction exitAction;
+    private AbstractAction f2cAction;   // convert Fahr. to Cels.
+    private AbstractAction c2fAction;   // convert Cels. to Fahr.
+    private AbstractAction clearAction; // clear F and C text fields
+    private AbstractAction exitAction;  // exit the program
 
 
+    // Note: Making these calculation functions static makes it possible to
+    // unit test them without the overhead of instantiating a Swing window.
+    // They should ideally be in a separate class altogether, but for the
+    // purposes of this illustration they're included here for convenience.
+
+    /**
+     * @return temperature in celsius given a fahrenheit value.
+     */
+    public static double calc_f2c(double fahr) {
+        return (5.0 / 9.0) * (fahr - 32.0);
+    }
+
+
+    /**
+     * @return temperature in fahrenheit given a celsius value.
+     */
+    public static double calc_c2f(double cels) {
+        return cels * (9.0 / 5.0) + 32.0;
+    }
+
+
+    /**
+     * @return true if, and only if, the string passed can successfully
+     * be converted to a floating point number (specifically, a Double).
+     */
+    private static boolean floatStringIsValid(String s) {
+
+        boolean valid = true;
+
+        try {
+            new Double(s);
+        } catch(NumberFormatException e) {
+            valid = false;
+        }
+
+        return valid;
+    }
+
+
+
+    /**
+     * Sole constructor, sets up frame with all components, centers on screen,
+     * and sets it up to exit the program when closed.
+     */
     public FrameInJava() {
         super("Fahrenheit <--> Celsius Converter");
-	initActions();
+        initActions();
         getContentPane().add(createConvertersPanel(), BorderLayout.CENTER);
         getContentPane().add(createButtonsPanel(),    BorderLayout.SOUTH);
-	setJMenuBar(createMenuBar());
+        setJMenuBar(createMenuBar());
         ((JPanel) getContentPane()).setBorder
-	        (BorderFactory.createEmptyBorder(12, 12, 12, 12));
-	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                (BorderFactory.createEmptyBorder(12, 12, 12, 12));
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         pack();
         centerOnScreen();
     }
 
 
-    
+    /**
+     * Sets up the Fahrenheit and Celsius text fields with their tooltips.
+     */
     private void createTextFields() {
 
-	fahrTextField = new JTextField(15);
-	celsTextField = new JTextField(15);
+        fahrTextField = new JTextField(15);
+        celsTextField = new JTextField(15);
 
-	String tooltip_text = "Input a temperature";
-	fahrTextField.setToolTipText(tooltip_text);
-	celsTextField.setToolTipText(tooltip_text);
+        String tooltip_text = "Input a temperature";
+        fahrTextField.setToolTipText(tooltip_text);
+        celsTextField.setToolTipText(tooltip_text);
     }
 
 
     /**
-     * Creates the panel containing the Fahrenheit and Celsius labels
-     * and text fields.
+     * @return a panel containing the Fahrenheit and Celsius labels
+     *         and text fields.
      */
     private JPanel createConvertersPanel() {
 
@@ -91,11 +137,11 @@ public class FrameInJava extends JFrame {
         labelPanel.add(new JLabel("Celsius:  "));
 
         JPanel textFieldPanel = new JPanel(new GridLayout(0, 1, 5, 5));
-	createTextFields();
+        createTextFields();
         textFieldPanel.add(fahrTextField);
         textFieldPanel.add(celsTextField);
 
-	setupDocumentListeners();	
+        setupDocumentListeners();
 
         JPanel panel = new JPanel(new BorderLayout());
         panel.add(labelPanel, BorderLayout.WEST);
@@ -107,26 +153,27 @@ public class FrameInJava extends JFrame {
 
 
     /**
-     * Creates menu bar with File, Edit, and Convert menus.
+     * @return menu bar populated with File, Edit, and Convert
+     *         menus with items
      */
     private JMenuBar createMenuBar() {
-    
-	JMenuBar menubar = new JMenuBar();
-    
-	JMenu file_menu = new JMenu("File");
-	file_menu.add(exitAction);
-	menubar.add(file_menu);
-    
-	JMenu edit_menu = new JMenu("Edit");
-	edit_menu.add(clearAction);
-	menubar.add(edit_menu);
-    
-	JMenu convert_menu = new JMenu("Convert");
-	convert_menu.add(f2cAction);
-	convert_menu.add(c2fAction);
-	menubar.add(convert_menu);
-    
-	return menubar;
+
+        JMenuBar menubar = new JMenuBar();
+
+        JMenu file_menu = new JMenu("File");
+        file_menu.add(exitAction);
+        menubar.add(file_menu);
+
+        JMenu edit_menu = new JMenu("Edit");
+        edit_menu.add(clearAction);
+        menubar.add(edit_menu);
+
+        JMenu convert_menu = new JMenu("Convert");
+        convert_menu.add(f2cAction);
+        convert_menu.add(c2fAction);
+        menubar.add(convert_menu);
+
+        return menubar;
     }
 
 
@@ -136,74 +183,76 @@ public class FrameInJava extends JFrame {
      */
     private void initActions() {
 
-	f2cAction    = new F2CAction();
-	c2fAction    = new C2FAction();
-	clearAction  = new ClearAction();
-	exitAction   = new ExitAction();
+        f2cAction    = new F2CAction();
+        c2fAction    = new C2FAction();
+        clearAction  = new ClearAction();
+        exitAction   = new ExitAction();
 
         // The initial state of the form will have nothing in the text fields.
         // Therefore, the conversion and clear actions are inappropriate and
-	// are set to disabled here.
-	//
-	// If the logic were more complex, and the desired enabled state could
-	// not be known at compile time, then we would find a way to call the
-	// same logic as the DocumentListener does, possibly by calling a
-	// DocumentListener method with null as the event parameter.
-	f2cAction.setEnabled(false);
-	c2fAction.setEnabled(false);
-	clearAction.setEnabled(false);
+        // are set to disabled here.
+        //
+        // If the logic were more complex, and the desired enabled state could
+        // not be known at compile time, then we would find a way to call the
+        // same logic as the DocumentListener does, possibly by calling a
+        // DocumentListener method with null as the event parameter.
+        f2cAction.setEnabled(false);
+        c2fAction.setEnabled(false);
+        clearAction.setEnabled(false);
     }
 
 
 
     /**
-     * Sets up text fields so that the actions are enabled only when appropriate.
+     * Sets up text fields with document listeners so that the actions
+     * are enabled only when appropriate.
      */
     private void setupDocumentListeners() {
 
-	// Will result in the F2C action being enabled only when the field's
-	// text can be parsed to a floating point number.
-	fahrTextField.getDocument().addDocumentListener(new SimpleDocumentListener() {
-	    public void handleDocumentEvent(DocumentEvent event) {
-		f2cAction.setEnabled(floatStringIsValid(fahrTextField.getText()));
-	    }
-	});
+        // Will result in the F2C action being enabled only when the field's
+        // text can be parsed to a floating point number.
+        fahrTextField.getDocument().addDocumentListener(new SimpleDocumentListener() {
+            public void handleDocumentEvent(DocumentEvent event) {
+                f2cAction.setEnabled(floatStringIsValid(fahrTextField.getText()));
+            }
+        });
 
-	// Will result in the C2F action being enabled only when the Celsius
-	// field's text can be parsed to a floating point number.
-	celsTextField.getDocument().addDocumentListener(new SimpleDocumentListener() {
-	    public void handleDocumentEvent(DocumentEvent event) {
-		c2fAction.setEnabled(floatStringIsValid(celsTextField.getText()));
-	    }
-	});
+        // Will result in the C2F action being enabled only when the Celsius
+        // field's text can be parsed to a floating point number.
+        celsTextField.getDocument().addDocumentListener(new SimpleDocumentListener() {
+            public void handleDocumentEvent(DocumentEvent event) {
+                c2fAction.setEnabled(floatStringIsValid(celsTextField.getText()));
+            }
+        });
 
 
-	// Will result in the Clear action being enabled only when there is
-	// any text in either text field.
-	DocumentListener clearDocumentListener = new SimpleDocumentListener() {
-	    public void handleDocumentEvent(DocumentEvent event) {
+        // Will result in the Clear action being enabled only when there is
+        // any text in either text field.
+        DocumentListener clearDocumentListener = new SimpleDocumentListener() {
+            public void handleDocumentEvent(DocumentEvent event) {
 
-		String ctext = celsTextField.getText();
-		String ftext = fahrTextField.getText();
+                String ctext = celsTextField.getText();
+                String ftext = fahrTextField.getText();
 
-		boolean should_enable =
-		    (ctext != null && ctext.length() > 0) ||
-		    (ftext != null && ftext.length() > 0);
+                boolean should_enable =
+                    (ctext != null && ctext.length() > 0) ||
+                    (ftext != null && ftext.length() > 0);
 
-		clearAction.setEnabled(should_enable);
-	    }
-	};
+                clearAction.setEnabled(should_enable);
+            }
+        };
 
-	fahrTextField.getDocument().addDocumentListener(clearDocumentListener);
-	celsTextField.getDocument().addDocumentListener(clearDocumentListener);
+        fahrTextField.getDocument().addDocumentListener(clearDocumentListener);
+        celsTextField.getDocument().addDocumentListener(clearDocumentListener);
     }
 
 
     /**
-     * Creates the button panel and lay out such that they will always
+     * @return the button panel laid out such that the buttons will always
      * stay at the right side of the window.
      */
     private JPanel createButtonsPanel() {
+
         JPanel innerPanel = new JPanel(new GridLayout(1, 0, 5, 5));
         innerPanel.add(new JButton(f2cAction));
         innerPanel.add(new JButton(c2fAction));
@@ -215,28 +264,28 @@ public class FrameInJava extends JFrame {
         outerPanel.setBorder(BorderFactory.createEmptyBorder(10, 0, 0, 0));
         return outerPanel;
     }
-    
+
 
     /**
      * Action to convert from Fahrenheit to Celsius that will be accessible
-     * via button, menu item, or Ctrl-t.
+     * via button, menu item, or Ctrl-s.
      */
     private class F2CAction extends AbstractAction {
 
         F2CAction() {
             super("Fahr -> Cels");
-	    putValue(Action.SHORT_DESCRIPTION, "Convert from Fahrenheit to Celsius");
-	    putValue(Action.ACCELERATOR_KEY,
-		     KeyStroke.getKeyStroke(KeyEvent.VK_S, Event.CTRL_MASK));
-
+            putValue(Action.SHORT_DESCRIPTION, "Convert from Fahrenheit to Celsius");
+            putValue(Action.ACCELERATOR_KEY,
+            KeyStroke.getKeyStroke(KeyEvent.VK_S, Event.CTRL_MASK));
         }
 
         public void actionPerformed(ActionEvent event) {
+
             String text = fahrTextField.getText();
             if (text != null && text.length() > 0)
             {
                 double fahr = Float.parseFloat(text);
-                double cels = (fahr - 32.0) * 5.0 / 9.0;
+                double cels = calc_f2c(fahr);
                 String celsText = Double.toString(cels);
                 celsTextField.setText(celsText);
             }
@@ -253,9 +302,9 @@ public class FrameInJava extends JFrame {
 
         C2FAction() {
             super("Cels -> Fahr");
-	    putValue(Action.SHORT_DESCRIPTION, "Convert from Celsius to Fahrenheit");
-	    putValue(Action.ACCELERATOR_KEY,
-		     KeyStroke.getKeyStroke(KeyEvent.VK_T, Event.CTRL_MASK));
+            putValue(Action.SHORT_DESCRIPTION, "Convert from Celsius to Fahrenheit");
+            putValue(Action.ACCELERATOR_KEY,
+            KeyStroke.getKeyStroke(KeyEvent.VK_T, Event.CTRL_MASK));
         }
 
         public void actionPerformed(ActionEvent event) {
@@ -263,7 +312,7 @@ public class FrameInJava extends JFrame {
             if (text != null && text.length() > 0)
             {
                 double cels = Float.parseFloat(text);
-                double fahr = (cels * 9.0 / 5.0) + 32.0;
+                double fahr = calc_c2f(cels);
                 String fahrText = Double.toString(fahr);
                 fahrTextField.setText(fahrText);
             }
@@ -280,9 +329,9 @@ public class FrameInJava extends JFrame {
 
         ClearAction() {
             super("Clear");
-	    putValue(Action.SHORT_DESCRIPTION, "Clear the temperature text fields");
-	    putValue(Action.ACCELERATOR_KEY,
-		     KeyStroke.getKeyStroke(KeyEvent.VK_L, Event.CTRL_MASK));
+            putValue(Action.SHORT_DESCRIPTION, "Clear the temperature text fields");
+            putValue(Action.ACCELERATOR_KEY,
+            KeyStroke.getKeyStroke(KeyEvent.VK_L, Event.CTRL_MASK));
         }
 
         public void actionPerformed(ActionEvent event) {
@@ -303,17 +352,17 @@ public class FrameInJava extends JFrame {
      */
     class ClearActionDocumentListener extends SimpleDocumentListener {
 
-	public void handleDocumentEvent(DocumentEvent event) {
+        public void handleDocumentEvent(DocumentEvent event) {
 
-	    String ctext = celsTextField.getText();
-	    String ftext = fahrTextField.getText();
+            String ctext = celsTextField.getText();
+            String ftext = fahrTextField.getText();
 
-	    boolean should_enable =
-		(ctext != null && ctext.length() > 0) ||
-		(ftext != null && ftext.length() > 0);
+            boolean should_enable =
+                    (ctext != null && ctext.length() > 0) ||
+                    (ftext != null && ftext.length() > 0);
 
-	    clearAction.setEnabled(should_enable);
-	}
+            clearAction.setEnabled(should_enable);
+        }
     }
 
 
@@ -325,32 +374,14 @@ public class FrameInJava extends JFrame {
 
         ExitAction() {
             super("Exit");
-	    putValue(Action.SHORT_DESCRIPTION, "Exit this program");
-	    putValue(Action.ACCELERATOR_KEY,
-		     KeyStroke.getKeyStroke(KeyEvent.VK_X, Event.CTRL_MASK));
+            putValue(Action.SHORT_DESCRIPTION, "Exit this program");
+            putValue(Action.ACCELERATOR_KEY,
+            KeyStroke.getKeyStroke(KeyEvent.VK_X, Event.CTRL_MASK));
         }
 
         public void actionPerformed(ActionEvent event) {
             System.exit(0);
         }
-    }
-
-
-    /**
-     * Returns true if, and only if, the string passed can successfully
-     * be converted to a floating point number (specifically, a Double).
-     */
-    private boolean floatStringIsValid(String s) {
-	
-	boolean valid = true;
-
-	try {
-	    new Double(s);
-	} catch(NumberFormatException e) {
-	    valid = false;
-	}
-
-	return valid;
     }
 
 
