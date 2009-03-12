5744fde82696f1ed1ede8396301a225868f8e2bd
Keith Bennett <krbofc@gmail.com>
2009-03-12 09:44:02 -0400

    Moved SimpleDocumentListener and SwingAction classes into main file.

--- src/FrameInRuby.rb
+++ src/FrameInRuby.rb
@@ -39,9 +39,9 @@ import javax.swing.KeyStroke
 # in Ruby, we need to "require" files, even though they are in the same
 # directory as the file being interpreted.
 
-require 'SwingAction'
-require 'SimpleDocumentListener'
-
+# =======================================================================
+# TemperatureConversion module
+# =======================================================================
 
 module TemperatureConversion
 
@@ -57,6 +57,10 @@ module TemperatureConversion
   end
 end
 
+
+# =======================================================================
+# FrameInRuby class
+# =======================================================================
 
 # Main application frame containing menus, text fields for the temperatures,
 # and buttons.  The menu items and buttons are driven by shared actions,
@@ -329,5 +333,110 @@ class FrameInRuby < JFrame
 end
 
 
+# =======================================================================
+# SimpleDocumentListener class
+# =======================================================================
+
+# Simple implementation of javax.swing.event.DocumentListener that
+# enables specifying a single code block that will be called
+# when any of the three DocumentListener methods are called.
+#
+# Note that unlike Java, where it is necessary to subclass the abstract
+# Java class SimpleDocumentListener, we can merely create an instance of
+# the Ruby class SimpleDocumentListener with the code block we want
+# executed when a DocumentEvent occurs.   This code can be in the form of
+# a code block, lambda, or proc.
+
+import javax.swing.event.DocumentListener
+
+
+class SimpleDocumentListener
+
+  # This is how we declare that this class implements the Java
+  # DocumentListener interface in JRuby:
+  include DocumentListener
+
+  attr_accessor :behavior
+
+  def initialize(&behavior)
+    self.behavior = behavior
+  end
+
+
+  def changedUpdate(event);  behavior.call event; end
+  def insertUpdate(event);   behavior.call event; end
+  def removeUpdate(event);   behavior.call event; end
+
+end
+
+
+# =======================================================================
+# SwingAction class
+# =======================================================================
+
+# When running FrameInRuby, this will generate a warning because
+# it is already imported in FrameInRuby.  In JRuby, unfortunately,
+# imports of Java classes are not confined to the file
+# in which they are specified; once you import a class,
+# it will be imported for other classes as well.
+# This may be fixed in a future version of JRuby.
+import javax.swing.AbstractAction
+
+
+# This class enables the specification of a Swing action
+# in a format natural to Ruby.
+#
+# It takes and stores a code block, lambda, or proc as the
+# action's behavior, so there is no need to define a new class
+# for each behavior. Also, it allows the optional specification
+# of the action's properties via the passing of hash entries,
+# which are effectively named parameters.
+class SwingAction < AbstractAction
+
+  attr_accessor :behavior
+
+
+# Creates the action object with a behavior, name, and options:
+#
+# behavior - a behavior can be a code block, lambda,
+# or a Proc.
+#
+# name - this is the name that will be used for the menu option,
+# button caption, etc.  Note that if an app is internationalized,
+# the name will vary by locale, so it is better to identify an action
+# by the action instance itself rather than its name.
+#
+# options - these are hash entries that will be passed to
+# AbstractAction.putValue().  Keys should be constants from the
+# javax.swing.Action interface, such as Action.SHORT_DESCRIPTION.
+# Ruby allows hash entries to passed as the last parameters to a
+# function, and they can be accessed inside the method as a single
+# hash object.
+#
+# Example:
+#
+# self.exit_action = SwingAction.new(
+#    "Exit",
+#     Action::SHORT_DESCRIPTION => "Exit this program",
+#     Action::ACCELERATOR_KEY =>
+#         KeyStroke.getKeyStroke(KeyEvent::VK_X, Event::CTRL_MASK)) do
+#       System.exit 0
+#     end
+#
+  def initialize(name, options=nil, &behavior)
+    super name
+    options.each { |key, value| putValue key, value } if options
+    self.behavior = behavior
+  end
+
+  def actionPerformed(action_event)
+    behavior.call action_event
+  end
+end
+
+
+# =======================================================================
+# Program entry point:
+# =======================================================================
 
 FrameInRuby.new.setVisible true
